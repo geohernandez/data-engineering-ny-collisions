5ca0ab03c6d2846d5c9b8cc43ea6ffc8835d8686
Geo Hernandez <geohernandez@gmail.com>
2023-04-26 18:44:46 +0200

    Updating new methods

--- dags/nyc_vehicles_collision.py
+++ dags/nyc_vehicles_collision.py
@@ -5,6 +5,7 @@
 
 import requests
 import os
+import logging
 
 # Packages for interacting with Azure Blob Storage
 from azure.identity import DefaultAzureCredential
@@ -25,8 +26,12 @@ def _get_nyc_collision_data(year,month,day,output_path):
         with open(filename, "w") as f:
             f.write(response.text)
 
-def get_blob_service_client_sas():
-    # TODO: Replace <storage-account-name> with your actual storage account name
+def _upload_nyc_collision_file_to_blob(container_name,filepath,filename):
+    logging.INFO(f"filename is {filename} and filepath is {filepath}")
+    blob_client = _get_blob_service_client_sas()
+    _upload_blob_file(container_name,filepath,filename)
+
+def _get_blob_service_client_sas():
     account_url = "https://nycdag.blob.core.windows.net/blobnycdata?sp=racwdl&st=2023-04-25T04:53:48Z&se=2025-04-01T12:53:48Z&spr=https&sv=2021-12-02&sr=c&sig=qAoUHBiulLnageE7jVWobGWmVOM%2BPQrfPreDyqucQEs%3D"
     # The SAS token string can be assigned to credential here or appended to the account URL
     # credential = sas_token
@@ -35,16 +40,7 @@ def get_blob_service_client_sas():
     blob_service_client = BlobServiceClient(account_url)
     return blob_service_client
 
-def upload_blob_data(container_name):
-    blob_service_client = get_blob_service_client_sas()
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob="sample-blob.txt")
-    data = b"Sample data for blob"
-
-    # Upload the blob data - default blob type is BlockBlob
-    blob_client.upload_blob(data, blob_type="BlockBlob")
-
-def upload_blob_file(container_name,filepath,filename):
-    blob_service_client = get_blob_service_client_sas()
+def _upload_blob_file(container_name,blob_service_client,filepath,filename):
     container_client = blob_service_client.get_container_client(container=container_name)
     with open(file=os.path.join(filepath, filename), mode="rb") as data:
         blob_client = container_client.upload_blob(name=filename,data=data, overwrite=True)
@@ -63,9 +59,18 @@ get_nyc_collision_data = PythonOperator(
     op_kwargs={
         "year": "{{ execution_date.year }}",
         "month": "{{ execution_date.month }}",
-        "day": "{{ execution_date.day }}",
-        "hour": "{{ execution_date.hour }}",
+        "day": "{{ execution_date.day}}",
         "output_path": "/tmp/"
+    }
+)
+
+upload_blob_file = PythonOperator(
+    task_id = "upload_blob_file",
+    python_callable=_upload_blob_file,
+    op_kwargs={
+    "container_name":"blobnycdata",
+    "filepath":"/tmp/",
+    "filename":"nyc_collision_{{yesterday_ds}}.txt"
     }
 )
 
